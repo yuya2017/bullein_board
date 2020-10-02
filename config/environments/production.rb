657cf26a30f906442794703345be5a151b0ce111
yuyakinoshita <yuya92220087@gmail.com>
2020-10-02 19:21:28 +0900

    メール設定を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,20 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'https://bulleinboard.herokuapp.com/' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    :address => "sv10775.xserver.jp",
+    :port => 587,
+    :user_name => "yuya92220087@gmail.com",
+    :password => "",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
